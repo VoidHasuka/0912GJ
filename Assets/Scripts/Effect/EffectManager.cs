282f8076dbcb714cd18d9cf3ddc3fae511ca6f55
VoidHasuka <1798707540@qq.com>
2025-09-13 13:21:26 +0800

    修改音效播放

--- Assets/Scripts/Effect/EffectManager.cs
+++ Assets/Scripts/Effect/EffectManager.cs
@@ -16,24 +16,24 @@ public class EffectManager
     //播放音符正确特效
     public void PlayCheckRightEffect(Vector2 position, UnityEngine.Color color = default)
     {
-        var checkRightEffectGo = GameObject.Instantiate(Resources.Load<GameObject>("Effect/CheckRightEffect"));
-        checkRightEffectGo.transform.position = position;
+        //var checkRightEffectGo = GameObject.Instantiate(Resources.Load<GameObject>("Effect/CheckRightEffect"));
+        //checkRightEffectGo.transform.position = position;
 
-        ParticleSystem ps = checkRightEffectGo.GetComponent<ParticleSystem>();
-        if (ps != null)
-        {
-            var main = ps.main;
-            main.startColor = new ParticleSystem.MinMaxGradient(color);
-            ps.Play();
-        }
+        //ParticleSystem ps = checkRightEffectGo.GetComponent<ParticleSystem>();
+        //if (ps != null)
+        //{
+        //    var main = ps.main;
+        //    main.startColor = new ParticleSystem.MinMaxGradient(color);
+        //    ps.Play();
+        //}
 
-        var renderer = ps.GetComponent<ParticleSystemRenderer>();
-        Material mat = Object.Instantiate(renderer.material);
-        renderer.material = mat;
+        //var renderer = ps.GetComponent<ParticleSystemRenderer>();
+        //Material mat = Object.Instantiate(renderer.material);
+        //renderer.material = mat;
 
-        mat.DOFade(0f, 1.0f).From(1f).OnComplete(() =>
-        {
-            Object.Destroy(ps.gameObject, 0.05f);
-        });
+        //mat.DOFade(0f, 1.0f).From(1f).OnComplete(() =>
+        //{
+        //    Object.Destroy(ps.gameObject, 0.05f);
+        //});
     }
 }

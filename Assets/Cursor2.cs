70a5ec5ee65ff9557258564a945ff1dc052939b4
yzl <127670643+HAHAKOook@users.noreply.github.com>
2025-09-14 13:05:30 +0800

    指针

--- Assets/Cursor2.cs
+++ Assets/Cursor2.cs
@@ -10,12 +10,19 @@ public class Cursor2 : MonoBehaviour
     void Start()
     {
 
-        this.GetComponent<Image>().rectTransform.DORotate(new Vector3(0,0,180f),0.5f);
+        this.GetComponent<Image>().rectTransform.DORotate(new Vector3(0, 0, 90f), 0.7f).SetEase(Ease.InCubic);
+        this.GetComponent<Image>().rectTransform.DOScale(new Vector3(0.5f, 0.5f, 0.5f), 0.5f).SetEase(Ease.OutSine);
+        this.GetComponent<Image>().DOColor(new Color(0, 0, 0,0), 0.7f).OnStepComplete(() => { Die(); });
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    
+    void Die()
+    {
+        GameObject.DestroyObject(this.gameObject); 
     }
 }
